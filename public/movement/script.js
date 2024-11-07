46ddc8f9c7b13fc9fc0a0fc2ebed03bb26c59a52
david20o7 <david.neacsu@icloud.com>
2024-11-07 23:11:50 +0000

    added storage for username
    Co-authored-by: vlad-tudor <vlad-tudor@users.noreply.github.com>

--- public/movement/script.js
+++ public/movement/script.js
@@ -2,23 +2,33 @@ import { Score } from "./Score.js";
 import { StaminaBar } from "./StaminaBar.js";
 
 import { Game } from "./Game.js";
+import { isValid } from "./utilities.js";
 
 const startGameScreen = document.querySelector("#startGameScreen");
 const startGameButton = document.querySelector("#startGameButton");
 const username = document.querySelector("#username");
+const gameOverMessage = document.querySelector("#gameOverMessage");
+
+username.value = localStorage.getItem("name");
+
+if (isValid(username.value)) {
+  startGameButton.classList.remove(["disabled"]);
+}
 
 username.addEventListener("input", (e) => {
   const name = e.target.value;
 
-  if (name.length > 3 && name.length < 16) {
+  if (isValid(name)) {
     startGameButton.classList.remove(["disabled"]);
   } else {
     startGameButton.classList.add(["disabled"]);
   }
+  gameOverMessage.innerText = `Sorry ${name} you have died`;
 });
 
 startGameButton.addEventListener("click", () => {
   startGameScreen.style.setProperty("display", "none");
+  localStorage.setItem("name", username.value);
   beginGame();
 });
 
