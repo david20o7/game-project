85d08d1f8b0c7c5fc9e980595e153e94d6694c89
david20o7 <david.neacsu@icloud.com>
2024-11-12 21:56:45 +0000

    stored highscore Co-authored-by: vlad-tudor <vlad-tudor@users.noreply.github.com>

--- public/movement/Score.js
+++ public/movement/Score.js
@@ -1,7 +1,7 @@
 export class Score {
   constructor() {
+    this.highestScore = parseInt(localStorage.getItem("highestScore")) || 0;
     this.currentScore = 0;
-    this.highestScore = 0;
     this.scoreElement = document.createElement("div");
     this.scoreElement.style.position = "absolute";
     this.scoreElement.style.top = "10px";
@@ -17,6 +17,7 @@ export class Score {
     this.currentScore = amount;
     if (this.currentScore > this.highestScore) {
       this.highestScore = this.currentScore;
+      localStorage.setItem("highestScore", this.highestScore);
     }
     this.updateScoreDisplay();
   }
---
+++ public/movement/index.js
@@ -0,0 +1,4 @@
+import * as utilities from "./utilities.js";
+import * as Player from "./Player.js";
+import * as Entity from "./Entity.js";
+import * as script from "./script.js";
